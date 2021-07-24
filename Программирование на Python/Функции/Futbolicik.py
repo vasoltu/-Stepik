9686ce1e1edea974cfb9c970ebb00d44a75247f1
vasoltu <45903076+vasoltu@users.noreply.github.com>
2021-07-24 14:11:07 +0300

    Add files via upload

--- Программирование на Python/Функции/Futbolicik.py
+++ Программирование на Python/Функции/Futbolicik.py
@@ -1,52 +1,52 @@
-n = input()    # 'Ввод'
-s = ''         # 'Создание пустой стоки'
+number_of_lines = input()    # 'Ввод'
+stroka = ''         # 'Создание пустой стоки'
 dict = {}      # 'Создание пустого словоря'
-for j in range(int(n)):    # '1 цикл(заполнение словаря)'
-    s = input()            # 'Ввод'
-    sp = s.split(';')      # 'Создание списка'
-    k = sp[0]              # 'Условия для 1 команды'
-    if k in dict:          # '1 условие(наличие ключа в словаре)'
-        dict[k][0] = dict[k][0] + 1
-        if int(sp[1]) > int(sp[3]):      # '1.1 условие(победа)'
-            dict[k][1] = dict[k][1] + 1
-            dict[k][4] = dict[k][4] + 3
-        elif int(sp[1]) == int(sp[3]):   # '1.2 условие(ничья)'
-            dict[k][2] = dict[k][2] + 1
-            dict[k][4] = dict[k][4] + 1
-        elif int(sp[1]) < int(sp[3]):    # '1.3 условие(поражение)'
-            dict[k][3] = dict[k][3] + 1
+for j in range(int(number_of_lines)):    # '1 цикл(заполнение словаря)'
+    stroka = input()            # 'Ввод'
+    spisok = stroka.split(';')      # 'Создание списка'
+    key_team1 = spisok[0]              # 'Условия для 1 команды'
+    if key_team1 in dict:          # '1 условие(наличие ключа в словаре)'
+        dict[key_team1][0] = dict[key_team1][0] + 1
+        if int(spisok[1]) > int(spisok[3]):      # '1.1 условие(победа)'
+            dict[key_team1][1] = dict[key_team1][1] + 1
+            dict[key_team1][4] = dict[key_team1][4] + 3
+        elif int(spisok[1]) == int(spisok[3]):   # '1.2 условие(ничья)'
+            dict[key_team1][2] = dict[key_team1][2] + 1
+            dict[key_team1][4] = dict[key_team1][4] + 1
+        elif int(spisok[1]) < int(spisok[3]):    # '1.3 условие(поражение)'
+            dict[key_team1][3] = dict[key_team1][3] + 1
     else:                                # '2 условие(не наличие ключа в словаре)'
-        dict.update({k: [1, 0, 0, 0, 0]})
-        if int(sp[1]) > int(sp[3]):      # '2.1 условие(победа)'
-            dict[k][1] = dict[k][1] + 1
-            dict[k][4] = dict[k][4] + 3
-        elif int(sp[1]) == int(sp[3]):   # '2.2 условие(ничья)'
-            dict[k][2] = dict[k][2] + 1
-            dict[k][4] = dict[k][4] + 1
-        elif int(sp[1]) < int(sp[3]):    # '2.3 условие(поражение)'
-            dict[k][3] = dict[k][3] + 1
+        dict.update({key_team1: [1, 0, 0, 0, 0]})
+        if int(spisok[1]) > int(spisok[3]):      # '2.1 условие(победа)'
+            dict[key_team1][1] = dict[key_team1][1] + 1
+            dict[key_team1][4] = dict[key_team1][4] + 3
+        elif int(spisok[1]) == int(spisok[3]):   # '2.2 условие(ничья)'
+            dict[key_team1][2] = dict[key_team1][2] + 1
+            dict[key_team1][4] = dict[key_team1][4] + 1
+        elif int(spisok[1]) < int(spisok[3]):    # '2.3 условие(поражение)'
+            dict[key_team1][3] = dict[key_team1][3] + 1
 
-    k1 = sp[2]                           # 'Условия для 2 команды'
-    if k1 in dict:                       # '1 условие(наличие ключа в словаре'
-        dict[k1][0] = dict[k1][0] + 1
-        if int(sp[3]) > int(sp[1]):      # '1.1 условие(победа)'
-            dict[k1][1] = dict[k1][1] + 1
-            dict[k1][4] = dict[k1][4] + 3
-        elif int(sp[3]) == int(sp[1]):   # '1.2 условие(ничья)'
-            dict[k1][2] = dict[k1][2] + 1
-            dict[k1][4] = dict[k1][4] + 1
-        elif int(sp[3]) < int(sp[1]):    # '1.3 условие(поражение)'
-            dict[k1][3] = dict[k1][3] + 1
+    key_team2 = spisok[2]                           # 'Условия для 2 команды'
+    if key_team2 in dict:                       # '1 условие(наличие ключа в словаре'
+        dict[key_team2][0] = dict[key_team2][0] + 1
+        if int(spisok[3]) > int(spisok[1]):      # '1.1 условие(победа)'
+            dict[key_team2][1] = dict[key_team2][1] + 1
+            dict[key_team2][4] = dict[key_team2][4] + 3
+        elif int(spisok[3]) == int(spisok[1]):   # '1.2 условие(ничья)'
+            dict[key_team2][2] = dict[key_team2][2] + 1
+            dict[key_team2][4] = dict[key_team2][4] + 1
+        elif int(spisok[3]) < int(spisok[1]):    # '1.3 условие(поражение)'
+            dict[key_team2][3] = dict[key_team2][3] + 1
     else:                                # '2 условие(не наличие ключа в словаре)'
-        dict.update({k1: [1, 0, 0, 0, 0]})
-        if int(sp[3]) > int(sp[1]):      # '2.1 условие(победа)'
-            dict[k1][1] = dict[k1][1] + 1
-            dict[k1][4] = dict[k1][4] + 3
-        elif int(sp[3]) == int(sp[1]):   # '2.2 условие(ничья)'
-            dict[k1][2] = dict[k1][2] + 1
-            dict[k1][4] = dict[k1][4] + 1
-        elif int(sp[3]) < int(sp[1]):    # '2.3 условие(поражение)'
-            dict[k1][3] = dict[k1][3] + 1
+        dict.update({key_team2: [1, 0, 0, 0, 0]})
+        if int(spisok[3]) > int(spisok[1]):      # '2.1 условие(победа)'
+            dict[key_team2][1] = dict[key_team2][1] + 1
+            dict[key_team2][4] = dict[key_team2][4] + 3
+        elif int(spisok[3]) == int(spisok[1]):   # '2.2 условие(ничья)'
+            dict[key_team2][2] = dict[key_team2][2] + 1
+            dict[key_team2][4] = dict[key_team2][4] + 1
+        elif int(spisok[3]) < int(spisok[1]):    # '2.3 условие(поражение)'
+            dict[key_team2][3] = dict[key_team2][3] + 1
 
 for com in dict.keys():                  # '2 цикл(вывод словаря)'
     print(com + ":" + str(dict[com][0]), str(dict[com][1]), str(dict[com][2]), str(dict[com][3]), str(dict[com][4]))
--- Программирование на Python/Функции/Функция modify_list(l).py
+++ Программирование на Python/Функции/Функция modify_list(l).py
@@ -1,10 +1,10 @@
 def modify_list(lst):          # 'Создание функции'
-    sp = len(lst)
+    spisok = len(lst)
     i = 0
-    while i < sp:              # '1 цикл(проход списка)'
+    while i < spisok:              # '1 цикл(проход списка)'
         if lst[i] % 2 != 0:    # '1 условие(нечетное число, удалить из списка)'
             del lst[i]
-            sp = sp - 1
+            spisok = spisok - 1
         elif lst[i] % 2 == 0:  # '2 условие(четное число, разделить на 2)'
             lst[i] = lst[i] // 2
             i = i + 1
